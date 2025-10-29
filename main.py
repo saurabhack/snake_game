103b54a31df15b110640454c06f53a11142a0cf9
saurabhack <kamanesaurabh68@gmail.com>
2025-10-29 18:57:11 +0530

    game is completed

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
 from turtle import Turtle,Screen
 import time
 from snake import Snake
+from food import Food
+from score_board import Scoreboard
 screen=Screen()
 screen.setup(width=600,height=600)
 
 screen.bgcolor("black")
 screen.title("My Snake game")
 screen.tracer(0)
-
+tim=Turtle()
 snake=Snake()
+food=Food()
 game_is_on=True
+scoreboard=Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down,"Down")
 screen.onkey(snake.left,"Left")
 screen.onkey(snake.right,"Right")
-
+counter=0
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    
+
+    #Detect collision with width
+    if snake.head.distance(food)<15:
+        print("nom nom nom")
+        counter=counter+1
+        print("score == ",counter)
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() >280 or snake.head.ycor() < -280:
+        game_is_on=False
+        scoreboard.game_over()
+    #detect collision with tail.
+    for segment in snake.segments:
+        if segment==snake.head:
+            pass
+        elif snake.head.distance(segment)<10:
+            game_is_on=False
+            scoreboard.game_over()
+        #if head collides with any segment in the tail
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -17,11 +17,20 @@ class Snake:
     
     def create_snake(self):
         for position in starting_position:
+            self.add_segment(position)
+
+    def add_segment(self,position):
             new_segment=Turtle(shape="square")
             new_segment.color("white")
             new_segment.penup()
             new_segment.goto(position)
             self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+
+
     def move(self):
         for seg_num in range(len(self.segments)-1,0,-1):
             new_x=self.segments[seg_num-1].xcor()
